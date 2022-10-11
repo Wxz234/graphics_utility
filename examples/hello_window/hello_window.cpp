f02ece416831c9d2a3eb0cdebd1b5da6087edd71
Wxz <429373960@qq.com>
2022-10-11 22:11:38 +0800

    update tutrorial

---
+++ examples/hello_window/hello_window.cpp
@@ -0,0 +1,50 @@
+#include <Windows.h>
+
+#include <donut/app/DeviceManager.h>
+#include <nvrhi/nvrhi.h>
+
+using namespace donut;
+
+class HelloWindow : public app::IRenderPass {
+public:
+	using IRenderPass::IRenderPass;
+
+	bool Init() {
+		return true;
+	}
+
+	void BackBufferResizing() override {}
+
+	void Animate(float fElapsedTimeSeconds) override {
+		GetDeviceManager()->SetInformativeWindowTitle("Hello Window");
+	}
+
+	void Render(nvrhi::IFramebuffer* framebuffer) override {}
+};
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+	app::DeviceManager* deviceManager = app::DeviceManager::Create(nvrhi::GraphicsAPI::D3D12);
+	app::DeviceCreationParameters deviceParams;
+#ifdef _DEBUG
+	deviceParams.enableDebugRuntime = true;
+	deviceParams.enableNvrhiValidationLayer = true;
+#endif
+	if (!deviceManager->CreateWindowDeviceAndSwapChain(deviceParams, "Hello Window"))
+	{
+		log::fatal("Cannot initialize a graphics device with the requested parameters");
+		return 1;
+	}
+
+	{
+		HelloWindow example(deviceManager);
+		if (example.Init())
+		{
+			deviceManager->AddRenderPassToBack(&example);
+			deviceManager->RunMessageLoop();
+			deviceManager->RemoveRenderPass(&example);
+		}
+	}
+	deviceManager->Shutdown();
+	delete deviceManager;
+	return 0;
+}
